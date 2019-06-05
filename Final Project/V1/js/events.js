628dfd0eed3e7d8a872ff7a8cfbc6feaed0e7cd0
Will <wberner@live.unc.edu>
2019-06-05 12:00:06 +0200

    More events added

--- Final Project/V1/js/events.js
+++ Final Project/V1/js/events.js
@@ -22,8 +22,8 @@ let someChoices1 = {
   choice2: "Go back to try to recover items"
 }
 chance();
-let result1 = (chanceValue < 60 ? "You luckily get out with a few belongings and rations for the journey ahead" : "You barely escape with your life, suffering burns. You see many slaughtered as you run away...");
-let startEvent = new Event(description1, q1, someChoices1, result1);
+let result1 = (chanceValue < 60 ? "You luckily get out with a few belongings and rations for the journey ahead." : "You barely escape with your life, suffering burns. You see many slaughtered as you run away...");
+let event1 = new Event(description1, q1, someChoices1, result1);
 
 /////////////////////////////////////////////////////////////////////
 
@@ -34,10 +34,21 @@ let someChoices2 = {
   choice2: "Keep moving forward"
 }
 chance();
-let result2 = (chanceValue < 80 ? "You follow the tracks to find a rabbit not too far along, enough to feed you for a day or two." : "You try to find the rabbit but are unsuccessful");
-let secondEvent = new Event(description2, q2, someChoices2, result2);
+let result2 = (chanceValue < 80 ? "You follow the tracks to find a rabbit not too far along, enough to feed you for a day or two." : "You try to find the rabbit but are unsuccessful.");
+let event2 = new Event(description2, q2, someChoices2, result2);
 
 /////////////////////////////////////////////////////////////////////
+
+let description3 = "Your people are hungry and you must eat."
+let q3 = "Do you eat a large meal or a small meal?";
+let someChoices3 = {
+  choice1: "Eat a large meal",
+  choice2: "Eat a small meal"
+}
+chance();
+let result3 = (chanceValue < 50 ? "The meal fills most people up and you continue to march on." : "Some eat but many go hungry...")
+let event3 = new Event(description3, q3, someChoices3, result3);
+
 
 function chance() {
   chanceValue = Phaser.Math.Between(0, 100);
